c74c84413e29b8fd78238df9b34595ee2c6d95fd
nathanielliganor <145381791+nathanielliganor@users.noreply.github.com>
2024-04-27 01:05:00 -0400

    Update Vis4.py

--- Vis4.py
+++ Vis4.py
@@ -9,7 +9,7 @@ from bokeh.io import output_notebook
 # Load the market data
 @st.cache
 def load_data():
-    df = pd.read_csv("~/Documents/MarketData/MarketData.csv")
+    df = pd.read_csv("./MarketData.csv")
     df['Date'] = pd.to_datetime(df['Date'])
     return df
 
