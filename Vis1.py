2a633f49f741371f5b724d9f4e23f56d76b1722f
nathanielliganor <145381791+nathanielliganor@users.noreply.github.com>
2024-04-27 02:46:18 -0400

    Update Vis1.py

--- Vis1.py
+++ Vis1.py
@@ -91,16 +91,14 @@ filtered_altair_df = df[df['Year'] == year_for_altair]
 
 # Create the Altair chart
 months_order = [calendar.month_name[i] for i in range(1, 13)]
-chart = alt.Chart(filtered_altair_df).mark_bar().encode(
+chart = alt.Chart(filtered_df).mark_bar().encode(
     x=alt.X("Month_Name:O", title="Month", sort=months_order, axis=alt.Axis(labelAngle=-45)),
+    xOffset=alt.XOffset("Ticker:N", title="Ticker"),
     y=alt.Y("sum(Volume):Q", title="Total Volume"),
     color=alt.Color("Ticker_Name:N", title="Ticker"),
-    column=alt.Column("Ticker_Name:N", title="Ticker"),  # This creates a separate column for each ticker
     tooltip=[alt.Tooltip('sum(Volume):Q', title="Volume Sum", format=',.0f')]
 ).properties(
-    title="Monthly Volume Sum for Each Ticker in Selected Year",
-    width=100,  # Set a smaller width for each bar chart facet
-    height=200
+    title="Monthly Volume Sum for Each Ticker in Selected Year"
 )
 
 st.altair_chart(chart, use_container_width=True)
